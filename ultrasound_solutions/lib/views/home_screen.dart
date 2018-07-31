c19b7c1293a4c05ce99821f9bc5fbb26211824cf
Brian Roper <appdevbri@gmail.com>
2018-07-31 00:12:47 -0400

    added quality guarantee to home screen

--- ultrasound_solutions/lib/views/home_screen.dart
+++ ultrasound_solutions/lib/views/home_screen.dart
@@ -4,6 +4,7 @@ import 'home_cell.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'service_form_screen.dart';
 import 'estimate_form_screen.dart';
+import 'package:ultrasound_solutions/models/colors.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -23,42 +24,69 @@ class HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return new Scaffold(
       body: new Center(
-        child: _buildHomeList(),
+        child: new Column(
+          children: <Widget>[
+            _buildHomeList(),
+            _buildGuarantee()
+          ],
+        ),
       ),
     );
   }
   
   Widget _buildHomeList(){
-    return new ListView.builder(
-      itemCount: listCount,
-      itemExtent: 125.0,
-      itemBuilder: (context, index) {
-        return new FlatButton(
-            onPressed: () {
-              //navigate to next screen
-              if(index == 0){
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => ServiceFormScreen()));
-              }
-              if(index == 1){
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => EstimateFormScreen()));
-              }
-              if(index == 2){
-                launch(phoneNumber);
-              }
-            },
-            child: new HomeCell(
-                titles[index],
-                icons[index],
-                colors[index]),
-          padding: new EdgeInsets.all(0.0),
-        );
-      });
+    return Expanded(
+      child: new ListView.builder(
+        itemCount: listCount,
+        itemExtent: 125.0,
+        itemBuilder: (context, index) {
+          return new FlatButton(
+              onPressed: () {
+                //navigate to next screen
+                if(index == 0){
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ServiceFormScreen()));
+                }
+                if(index == 1){
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => EstimateFormScreen()));
+                }
+                if(index == 2){
+                  launch(phoneNumber);
+                }
+              },
+              child: new HomeCell(
+                  titles[index],
+                  icons[index],
+                  colors[index]),
+            padding: new EdgeInsets.all(0.0),
+          );
+        }),
+    );
+  }
+  
+  Widget _buildGuarantee(){
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      child: new Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          new Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Icon(Icons.beenhere, size: 34.0, color: kUltraSoundPrimaryText),
+              new Text("Quality Guarantee", style: new TextStyle(fontSize: 16.0, color: kUltraSoundPrimaryText, fontWeight: FontWeight.bold))
+            ],
+          ),
+          new Text("You!, our customer are guaranteed complete satisfaction. If for any reason, you are not completley satisfied with your experience please let us know so it can be rectified. Thank you for your business!", style: new TextStyle(fontSize: 16.0, color: kUltraSoundPrimaryText))
+        ],
+      ),
+    );
   }
 }
 
